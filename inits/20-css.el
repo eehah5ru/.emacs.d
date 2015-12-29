3d881f8f8f6381a7e3ef6bd1ede518b95d322964
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2015-12-29 13:20:41 +0300

    added css related inits, added desktop-save

--- inits/20-css.el
+++ inits/20-css.el
@@ -1,26 +1,30 @@
+;; (require-or-install 'css)
+(require-or-install 'scss-mode)
+(require-or-install 'sass-mode)
+
 (autoload 'css-mode "css-mode" nil t)
+
 (eval-after-load "css-mode"
   '(progn
      (setq cssm-indent-function 'cssm-c-style-indenter)
      (setq css-indent-offset 2)))
 
-(unless (package-installed-p 'less-css-mode)
-  (package-install 'less-css-mode))
-(autoload 'less-css-mode "less-css-mode" nil t)
+;; (unless (package-installed-p 'less-css-mode)
+;;   (package-install 'less-css-mode))
+;; (autoload 'less-css-mode "less-css-mode" nil t)
 
-(unless (package-installed-p 'scss-mode)
-  (package-install 'scss-mode))
+;; (unless (package-installed-p 'scss-mode)
+;;   (package-install 'scss-mode))
 (autoload 'scss-mode "scss-mode" nil t)
 (eval-after-load "scss-mode"
   '(progn
      (setq scss-compile-at-save nil)))
 
 (defun my-scss-mode-hook ()
-  "Hooks for Web mode."
+  "Hooks for SCSS mode."
   (setq scss-mode-markup-indent-offset 2))
 
 (add-hook 'scss-mode-hook  'my-scss-mode-hook)
 
 (add-hook 'sass-mode-hook
-	  (lambda () (highlight-indentation-current-column-mode)))
-
+    (lambda () (highlight-indentation-current-column-mode)))
