4d169fb803608503a3753a98dabfd3899ed481d5
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2016-05-05 20:03:36 +0300

    added support for org mode

--- inits/20-org-mode.el
+++ inits/20-org-mode.el
@@ -4,7 +4,45 @@
 ;;
 ;;
 (require 'org-install)
+;; (require-or-install 'org-bullets)
+;; (setq org-hide-emphasis-markers t)
+
+;; (font-lock-add-keywords 'org-mode
+;;                         '(("^ +\\([-*]\\) "
+;;                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+;;                              ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+;;                              ((x-list-fonts "Verdana")         '(:font "Verdana"))
+;;                              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+;;                              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+;;        (base-font-color     (face-foreground 'default nil 'default))
+;;        (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+;;   (custom-theme-set-faces 'user
+;;                           `(org-level-8 ((t (,@headline ,@variable-tuple))))
+;;                           `(org-level-7 ((t (,@headline ,@variable-tuple))))
+;;                           `(org-level-6 ((t (,@headline ,@variable-tuple))))
+;;                           `(org-level-5 ((t (,@headline ,@variable-tuple))))
+;;                           `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+;;                           `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+;;                           `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+;;                           `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+;;                           `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
+
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
+(add-hook 'org-mode-hook
+          '(lambda() (set-fill-column 80)))
+
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
+(define-key org-mode-map (kbd "C-c l") 'org-store-link)
+(define-key org-mode-map (kbd "C-c a") 'org-agenda)
+(define-key org-mode-map (kbd "C-ф") 'org-beginning-of-line)
+(define-key org-mode-map (kbd "C-у") 'org-end-of-line)
+(define-key org-mode-map (kbd "C-н") 'org-yank)
+(define-key org-mode-map (kbd "C-с M-ц") 'org-copy)
+(define-key org-mode-map (kbd "M-ц") 'kill-ring-save)
+(define-key org-mode-map (kbd "C-п") 'keyboard-quit)
 (setq org-log-done t)
