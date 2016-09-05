275c9c63a06b43089594ee305e4776fef3ba5021
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2016-09-06 02:05:40 +0300

    improved aren mode

--- inits/10-paren.el
+++ inits/10-paren.el
@@ -1,2 +1,24 @@
 (require-or-install 'paren)
+
+;; (require 'smartparens-config)
+
 (show-paren-mode 1)
+
+;;;
+;;; disable paren mode for buffer
+;;;
+(defun endless/locally-disable-show-paren ()
+  (interactive)
+  (setq-local show-paren-mode nil))
+
+(defun endless/locally-enable-show-paren ()
+  (interactive)
+  (setq-local show-paren-mode 1))
+
+
+;;;
+;;; keep only basic show-paren mode support
+;;;
+(defun endless/locally-keep-only-basic-paren ()
+  (interactive)
+  (setq-local show-paren-data-function #'show-paren--default))
