223622f055140d7a8f3072a3a6bed0a2fd75a8e8
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2016-12-27 15:48:27 +0300

    [solidity] changed style of comments -> //

--- inits/20-solidity.el
+++ inits/20-solidity.el
@@ -1,2 +1,8 @@
 (require-or-install 'solidity-mode)
 (require-or-install 'flymake-solidity)
+
+
+
+(add-hook 'solidity-mode-hook
+          (lambda () (setq comment-start "//"
+                      comment-end   "")))
