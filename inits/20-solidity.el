c842a0869d5d49cadd28f1a3187215b7f84846e6
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2016-12-27 19:53:51 +0300

    added support for flycheck via solc

--- inits/20-solidity.el
+++ inits/20-solidity.el
@@ -1,8 +1,27 @@
+;;;
+;;; provide path to solc binary
+;;; it should be in the SOLC_BINARY ENV var
+(if (not (getenv "TERM_PROGRAM"))
+    (setq solidity-solc-path
+          (shell-command-to-string "source $HOME/.zshrc && printf $SOLC_BINARY")))
+
+(if (not solidity-solc-path)
+    (message "you should set SOLC_BINARY in $HOME/.zshrc"))
+
+
+
+
+
+;;;
+;;; require necessary plugins
+;;;
 (require-or-install 'solidity-mode)
 (require-or-install 'flymake-solidity)
 
 
-
+;;;
+;;; hooks
+;;;
 (add-hook 'solidity-mode-hook
           (lambda () (setq comment-start "//"
                       comment-end   "")))
