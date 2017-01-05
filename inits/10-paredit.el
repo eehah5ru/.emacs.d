9191bfd9b5bb77074617ee286723af866eff95c8
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-01-05 20:48:39 +0300

    [paredit] fixed wrong keybindings for wind switching

--- inits/10-paredit.el
+++ inits/10-paredit.el
@@ -8,10 +8,11 @@
 (defun my-paredit-mode-config ()
   "modify keymaps in paredit mode"
   ;; remove bindings
-  (local-unset-key (kbd "M-<up>"))
-  (local-unset-key (kbd "M-<down>"))
-  ;; add new ones
-  (local-set-key (kbd "M-<up>") 'windmove-up)
-  (local-set-key (kbd "M-<down>") 'windmove-down))
+  ;; (local-unset-key (kbd "M-<up>"))
+  ;; (local-unset-key (kbd "M-<down>"))
+  ;; ;; add new ones
+  ;; (local-set-key (kbd "M-<up>") 'windmove-up)
+  ;; (local-set-key (kbd "M-<down>") 'windmove-down)
+  (local-set-key (kbd "C-k") 'paredit-kill))
 
 (add-hook 'paredit-mode-hook 'my-paredit-mode-config)
