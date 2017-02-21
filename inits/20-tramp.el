24cb205c8e238eddf0a5863856646fa91801edae
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-02-21 17:00:03 +0300

    [tramp] fixed problems with some remote ssh's

--- inits/20-tramp.el
+++ inits/20-tramp.el
@@ -5,3 +5,7 @@
 ;;
 ;; (require 'tramp)
 ;; (setq tramp-shell-prompt-pattern "^[^$>\n]*[#$%>] *\\(\[[0-9;]*[a-zA-Z] *\\)*")
+
+(setq tramp-shell-prompt-pattern "\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>].* *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*")
+
+;; (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
