8c78c89e819c370bf74bf28ec1661acf119f25dc
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-02-25 19:57:51 +0300

    [tramp] helm-tramp supprt

--- inits/20-tramp.el
+++ inits/20-tramp.el
@@ -6,6 +6,7 @@
 ;; (require 'tramp)
 ;; (setq tramp-shell-prompt-pattern "^[^$>\n]*[#$%>] *\\(\[[0-9;]*[a-zA-Z] *\\)*")
 
+(require-or-install 'helm-tramp)
 (setq tramp-shell-prompt-pattern "\\(?:^\\|\r\\)[^]#$%>\n]*#?[]#$%>].* *\\(^[\\[[0-9;]*[a-zA-Z] *\\)*")
 
 ;; (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
