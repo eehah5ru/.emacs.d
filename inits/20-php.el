dbd090a4d9be7a4c83df665f17a13fe18066856c
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-02-28 18:59:10 +0300

    [php] config added

---
+++ inits/20-php.el
@@ -0,0 +1,18 @@
+;; (require-or-install 'psgml)
+
+;; (require-or-install 'mmm-mode)
+
+;; (setq mmm-global-mode 'maybe)
+
+;; (mmm-add-mode-ext-class 'web-mode "\\.php3?\\'" 'html-php)
+;; (mmm-add-mode-ext-class 'web-mode "\\.php?\\'" 'html-php)
+
+;; (mmm-add-classes
+;;  '((html-php
+;;     :submode php-mode
+;;     :front "<\\?\\(php\\)?"
+;;     :back "\\?>")))
+;; (autoload 'php-mode "php-mode" "PHP editing mode" t)
+
+(add-to-list 'auto-mode-alist '("\\.php3?\\'" . web-mode) t)
+(add-to-list 'auto-mode-alist '("\\.php?\\'" . web-mode) t)
