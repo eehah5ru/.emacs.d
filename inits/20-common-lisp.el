725f09afb884a1766952a42b0aa1ea2a3e83d45a
eehah5ru <nicola.spesivcev@gmail.com>
2024-03-30 12:32:18 +0100

    [CL] fix deps

--- inits/20-common-lisp.el
+++ inits/20-common-lisp.el
@@ -3,80 +3,83 @@
 ;;; doc live here
 ;;; https://lispcookbook.github.io/cl-cookbook/emacs-ide.html
 ;;;
+(use-package slime-autoloads
+             :ensure t)    
+
 (use-package slime
-  :init
-  (progn
-    (add-hook 'slime-mode-hook
-              (lambda ()
-                (unless (slime-connected-p)
-                  (save-excursion (slime)))))
-    ;; (add-hook 'slime-mode-hook
-    ;;           (lambda ()
-    ;;             )
-    ;;  )
-    )
-  :config
-  (progn
-    (use-package 'slime-autoloads)    
-    (load (expand-file-name "~/.roswell/helper.el"))
-    (setq inferior-lisp-program "ros -Q run")
-    (load "~/.roswell/lisp/quicklisp/log4slime-setup.el")
-    (global-log4slime-mode 1)
-    ;; (setf inferior-lisp-program "sbcl")
-    (slime-setup '(slime-fancy
-                   slime-quicklisp
-                   slime-asdf
-                   helm-slime
-                   slime-indentation
-                   slime-tramp
-                   slime-banner
-                   slime-company
-                   slime-mdot-fu
-                   ;; slime-hyperspec-lookup
-                   slime-repl))
-    (setq slime-net-coding-system 'utf-8-unix)
-    (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
-    (setq slime-auto-select-connection 'always)
-    (setq slime-kill-without-query-p t)
-    (setq slime-fuzzy-explanation "")
-    (setq slime-asdf-collect-notes t)
-    (setq slime-inhibit-pipelining nil)
-    (setq slime-load-failed-fasl 'always)
-    (setq slime-repl-history-remove-duplicates t)
-    (setq slime-repl-history-trim-whitespaces t)
-    (setq slime-export-symbol-representation-auto t)
-    (setq lisp-indent-function 'common-lisp-indent-function)
-    (setq lisp-loop-indent-subclauses nil)
-    (setq lisp-loop-indent-forms-like-keywords t)
-    (setq lisp-lambda-list-keyword-parameter-alignment t)
+             :init
+             (progn
+               (require 'slime-autoloads)
+               (add-hook 'slime-mode-hook
+                         (lambda ()
+                           (unless (slime-connected-p)
+                             (save-excursion (slime)))))
+               ;; (add-hook 'slime-mode-hook
+               ;;           (lambda ()
+               ;;             )
+               ;;  )
+               )
+             :config
+             (progn
+               (load (expand-file-name "~/.roswell/helper.el"))
+               (setq inferior-lisp-program "ros -Q run")
+               (load "~/.roswell/lisp/quicklisp/log4slime-setup.el")
+               (global-log4slime-mode 1)
+               ;; (setf inferior-lisp-program "sbcl")
+               (slime-setup '(slime-fancy
+                              slime-quicklisp
+                              slime-asdf
+                              helm-slime
+                              slime-indentation
+                              slime-tramp
+                              slime-banner
+                              slime-company
+                              slime-mdot-fu
+                              ;; slime-hyperspec-lookup
+                              slime-repl))
+               (setq slime-net-coding-system 'utf-8-unix)
+               (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
+               (setq slime-auto-select-connection 'always)
+               (setq slime-kill-without-query-p t)
+               (setq slime-fuzzy-explanation "")
+               (setq slime-asdf-collect-notes t)
+               (setq slime-inhibit-pipelining nil)
+               (setq slime-load-failed-fasl 'always)
+               (setq slime-repl-history-remove-duplicates t)
+               (setq slime-repl-history-trim-whitespaces t)
+               (setq slime-export-symbol-representation-auto t)
+               (setq lisp-indent-function 'common-lisp-indent-function)
+               (setq lisp-loop-indent-subclauses nil)
+               (setq lisp-loop-indent-forms-like-keywords t)
+               (setq lisp-lambda-list-keyword-parameter-alignment t)
 
-    ;; keys
+               ;; keys
 
-    (defun slime-repl-return-at-end ()
-      (interactive)
-      (if (<= (point-max) (point))
-          (slime-repl-return)
-        (slime-repl-newline-and-indent)))
+               (defun slime-repl-return-at-end ()
+                 (interactive)
+                 (if (<= (point-max) (point))
+                     (slime-repl-return)
+                     (slime-repl-newline-and-indent)))
 
-    (define-key lisp-mode-map
-                (kbd "C-c C-q")
-                'slime-close-all-parens-in-sexp)
-    (define-key slime-mode-indirect-map
-                (kbd "M-_")
-                'paredit-convolute-sexp)
-    (define-key slime-repl-mode-map
-                (kbd "C-c C-z")
-                #'quit-window)
-    (define-key slime-repl-mode-map
-                (read-kbd-macro paredit-backward-delete-key)
-                nil)
-  (define-key slime-repl-mode-map
-              (kbd "RET")
-              'slime-repl-return-at-end)
-  (define-key slime-repl-mode-map
-              (kbd "<return>")
-              'slime-repl-return-at-end)
-    ))
+               (define-key lisp-mode-map
+                   (kbd "C-c C-q")
+                 'slime-close-all-parens-in-sexp)
+               (define-key slime-mode-indirect-map
+                   (kbd "M-_")
+                 'paredit-convolute-sexp)
+               (define-key slime-repl-mode-map
+                   (kbd "C-c C-z")
+                 #'quit-window)
+               (define-key slime-repl-mode-map
+                   (read-kbd-macro paredit-backward-delete-key)
+                 nil)
+               (define-key slime-repl-mode-map
+                   (kbd "RET")
+                 'slime-repl-return-at-end)
+               (define-key slime-repl-mode-map
+                   (kbd "<return>")
+                 'slime-repl-return-at-end)
+               ))
 
 (use-package slime-company
   :ensure t)
