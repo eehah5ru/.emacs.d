c1b138832f067b7f2c0b6c42fe61e5bdf1df1a5e
eehah5ru <nicola.spesivcev@gmail.com>
2026-09-08 12:55:15 +0200

    [claude] change

--- inits/30-claude.el
+++ inits/30-claude.el
@@ -1,8 +1,8 @@
 ;; Load PATH from shell
 (use-package exec-path-from-shell
-  :ensure t
-  :if (memq window-system '(mac ns x))
-  :config (exec-path-from-shell-initialize))
+             :ensure t
+             :if (memq window-system '(mac ns x))
+             :config (exec-path-from-shell-initialize))
 
 ;; Git Interface
 ;; (use-package magit
@@ -14,19 +14,19 @@
 ;;   :ensure t)
 
 (use-package ghostel
-  :ensure t)
+             :ensure t)
 
 ;; Claude Code Agent Setup
-(use-package claude-code-ide
-  :vc (:url "https://github.com/manzaltu/claude-code-ide.el" :rev :newest)
-  :bind ("C-c c" . claude-code-ide-menu)
-  :config
-  (setq claude-code-ide-terminal-backend 'ghostel)
-  (setq claude-code-ide-enable-mcp-server t)
-  (claude-code-ide-emacs-tools-setup))
+;; (use-package claude-code-ide
+;;   :vc (:url "https://github.com/manzaltu/claude-code-ide.el" :rev :newest)
+;;   :bind ("C-c c" . claude-code-ide-menu)
+;;   :config
+;;   (setq claude-code-ide-terminal-backend 'ghostel)
+;;   (setq claude-code-ide-enable-mcp-server t)
+;;   (claude-code-ide-emacs-tools-setup))
 
 (use-package claude-code-ide
-  :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
-  :bind ("C-c C-'" . claude-code-ide-menu) ; Set your favorite keybinding
-  :config
-  (claude-code-ide-emacs-tools-setup))
+             :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
+             :bind ("C-c C-'" . claude-code-ide-menu) ; Set your favorite keybinding
+             :config
+             (claude-code-ide-emacs-tools-setup))
